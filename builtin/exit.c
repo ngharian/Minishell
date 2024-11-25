d10f986157fb8707d1d3961c511712cfaf33776b
Gauthier Dero <gdero@shi-c1-p4.s19.be>
2024-11-25 15:15:22 +0100

    exit

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,17 +6,17 @@
 /*   By: gdero <gdero@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 19:12:28 by gdero             #+#    #+#             */
-/*   Updated: 2024/11/25 14:03:59 by gdero            ###   ########.fr       */
+/*   Updated: 2024/11/25 14:49:55 by gdero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h" 
 
-int	ft_exit(t_commands *cmd, char *lower_cmd, t_env_vars *vars) // exit 123 coucou n'exit pas --> PAS FINI
+int	ft_exit(t_commands *cmd, t_env_vars *vars) // exit 123 coucou n'exit pas --> PAS FINI
 {
 	int	index;
-	if (cmd->previous != NULL || cmd->next != NULL) //rentre la dedans quand exit seul ???
-			return (7);
+	/*if (cmd->previous != NULL || cmd->next != NULL) //rentre la dedans quand exit seul ???
+			return (7);*/
 	if (cmd->cmd[1])
 	{
 		index = -1;
@@ -24,20 +24,19 @@ int	ft_exit(t_commands *cmd, char *lower_cmd, t_env_vars *vars) // exit 123 couc
 		{
 			if (!ft_isdigit(cmd->cmd[1][index]))
 			{
-				printf("minishell: exit: numeric argument required\n");
-				break ;
+				printf("exit\nminishell: exit: numeric argument required\n");
+				exit(255);
 			}
 		}
 		if (cmd->cmd[2])
-			printf("minishell: exit: too many arguments\n");
-		return (7);
+		{
+			printf("exit\nminishell: exit: too many arguments\n"); //!!! echo $? = 1
+			vars->exit_code = 1;
+			return (7);			
+		}
 	}
-	else
-	{
-		free_struct(&cmd, 0);
-		free_env(vars, 0);
-		free(lower_cmd);
-		printf("exit\n");
-		exit(0);
-	}
+	if (cmd->cmd[1]) //!!! overflow
+		vars->exit_code = ft_atoi(cmd->cmd[1]);
+	printf("exit\n");
+	exit((unsigned char)vars->exit_code);
 }
